4ad724c3ccc287b50010809faac88ecafe6c5306
Adam Zimowski <mrazjava@yandex.com>
2019-09-24 13:26:36 +0200

    reading employee list dynamically from JS
    https://www.taniarascia.com/getting-started-with-vue/

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
 
-    <employee-table />
+    <employee-table :employees="employees" />
   </div>
 </template>
 
@@ -14,6 +14,28 @@
     components: {
       EmployeeTable,
     },
+    data() {
+      return {
+        employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
+  },
+
   }
 </script>
 
